c7e1922f25d7495f2efc84e6accdfa32655b80f6
ikraP <ikrapervaiz@gmail.com>
2020-02-03 14:10:21 +0000

    adding cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const apiRouter = require("./routers /apiRouter");
+const cors = require("cors");
 const {
   send404Error,
   sendCustomError,
@@ -9,6 +10,7 @@ const {
 } = require("./error_functions/errors");
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api", apiRouter);
 
